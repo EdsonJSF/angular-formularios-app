3127512402dd06876ae2826e56d9f48a9cc40107
Edsonjsf <edsonjsf1990@gmail.com>
2023-01-24 14:04:41 -0400

    Acceso a las rutas desde el side menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SideMenuComponent } from './side-menu/side-menu.component';
 
 const importExport = [SideMenuComponent];
 
 @NgModule({
   declarations: [importExport],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [importExport],
 })
 export class SharedModule {}
--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  title: string;
+  path: string;
+}
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss']
+  styleUrls: ['./side-menu.component.scss'],
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent {
+  templateMenu: MenuItem[] = [
+    {
+      title: 'Básicos',
+      path: './template/basicos',
+    },
+    {
+      title: 'Dinámicos',
+      path: './template/dinamicos',
+    },
+    {
+      title: 'Switches',
+      path: './template/switches',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  reactiveMenu: MenuItem[] = [
+    {
+      title: 'Básicos',
+      path: './reactive/basicos',
+    },
+    {
+      title: 'Dinámicos',
+      path: './reactive/dinamicos',
+    },
+    {
+      title: 'Switches',
+      path: './reactive/switches',
+    },
+  ];
 }

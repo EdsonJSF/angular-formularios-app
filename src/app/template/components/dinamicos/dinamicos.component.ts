2535f13809a647523aa792f9167ea541bcdc6c2d
Edsonjsf <edsonjsf1990@gmail.com>
2023-01-30 13:27:07 -0400

    Tamplate Form - Dinamicos: Creacion del formulario con elementos dinamicos

--- src/app/template/components/dinamicos/dinamicos.component.ts
+++ src/app/template/components/dinamicos/dinamicos.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styleUrls: ['./dinamicos.component.scss']
+  styleUrls: ['./dinamicos.component.scss'],
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  persona: Persona = {
+    nombre: 'Edson',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'Metal Gear 2' },
+    ],
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  guardar() {
+    console.log('sad');
   }
-
 }

1a1e473efb4f527947b613d7eb4c733f72e6f465
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-06 14:16:50 -0400

    Reactive Form - Dinamicos: Validar campos

--- src/app/reactive/components/dinamicos/dinamicos.component.ts
+++ src/app/reactive/components/dinamicos/dinamicos.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styleUrls: ['./dinamicos.component.scss']
+  styleUrls: ['./dinamicos.component.scss'],
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  validForm(nameField: string) {
+    return (
+      this.miFormulario.controls[nameField].errors &&
+      this.miFormulario.controls[nameField].touched
+    );
   }
 
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+  }
 }

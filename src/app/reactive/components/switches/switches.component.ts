ec9c29456f4fd3510f3f5eac819dc753516908b3
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-08 14:05:21 -0400

    Reactive Form - Switches: Creacion del formulario con FormBuilder

--- src/app/reactive/components/switches/switches.component.ts
+++ src/app/reactive/components/switches/switches.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styleUrls: ['./switches.component.scss']
+  styleUrls: ['./switches.component.scss'],
 })
 export class SwitchesComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    genero: [, Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [, Validators.requiredTrue],
+  });
 
-  constructor() { }
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.miFormulario.reset({ ...this.persona, condiciones: false });
   }
-
 }

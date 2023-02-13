da66a0ee21cc2f3afe445da4697307e2e9420c94
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-13 13:38:37 -0400

    Pagina de Registro: En el email validar contra una expresión regular

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -9,13 +9,20 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegistroComponent implements OnInit {
   // TODO: Temporal
   nombrePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   miFormulario: FormGroup = this.fb.group({
     nombre: [, [Validators.required, Validators.pattern(this.nombrePattern)]],
+    email: [, [Validators.required, Validators.pattern(this.emailPattern)]],
   });
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Edson Sanchez',
+      email: 'edson@edson.com',
+    });
+  }
 
   validForm(nameField: string) {
     return (

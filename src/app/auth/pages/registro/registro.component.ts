2b07eb2b7d95bc27954255f4ac3fac9ccc3ef9bf
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-13 14:22:19 -0400

    Pagina de Registro: Separar la lógica de validaciones del componente con TS o con un Serv

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormBuilder,
-  Validators,
-  FormControl,
-} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { ValidatorsService } from '../../../shared/validators/validators.service';
+import { emailPattern, noPuedeSerStrider, nombrePattern } from 'src/app/shared/validators/validations';
 
 @Component({
   selector: 'app-registro',
@@ -12,26 +10,12 @@ import {
   styleUrls: ['./registro.component.scss'],
 })
 export class RegistroComponent implements OnInit {
-  // TODO: Temporal
-  nombrePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
-  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
-
-  noPuedeSerStrider(control: FormControl) {
-    const valor = control.value?.trim().toLowerCase();
-    if (valor === 'strider') {
-      return {
-        noStrider: true,
-      };
-    }
-    return null;
-  }
-
   miFormulario: FormGroup = this.fb.group({
-    nombre: [, [Validators.required, Validators.pattern(this.nombrePattern)]],
-    email: [, [Validators.required, Validators.pattern(this.emailPattern)]],
-    username: [, [Validators.required, this.noPuedeSerStrider]],
+    nombre: [, [Validators.required, Validators.pattern(this.validServ.nombrePattern)]],
+    email: [, [Validators.required, Validators.pattern(this.validServ.emailPattern)]],
+    username: [, [Validators.required, this.validServ.noPuedeSerStrider]],
   });
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private validServ: ValidatorsService) {}
 
   ngOnInit(): void {
     this.miFormulario.reset({
